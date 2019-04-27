0ba5eb5eb53adc12c8769e0d282b3b0e6b251adf
lihaineng <1085436468@qq.com>
2019-04-27 17:27:36 +0800

    初始化

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,15 +1,5 @@
 from queue import Queue
 
-"""
-调度器模块: 1. 缓冲请求, 2 请求去重
-
-步骤:
- 1. 初始一个队列, 用于缓存请求
- 2. 提供添加请求的方法
- 3. 提取获取请求的方法
- 4. 请求去重的方法
-"""
-
 class Scheduler(object):
 
     def __init__(self):
